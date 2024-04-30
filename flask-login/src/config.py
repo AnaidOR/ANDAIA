53547b508bd3c6ec25134bd709e8309aae7810b4
Anaid Olguin <aniiop03@gmail.com>
2024-04-30 17:22:28 -0600

    cambios

--- flask-login/src/config.py
+++ flask-login/src/config.py
@@ -1,5 +1,13 @@
-class DevelopmentConfig():
-    DEBUG=True
+class config:
+    SECRET_KEY ='123'
+
+class DevelopmentConfig(config):
+    
+    DEBUG = True
+    MYSQL_HOST ='localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB ='andaia'
 
 config={
     'development':DevelopmentConfig
---
+++ flask-login/src/models/ModelUser.py
@@ -0,0 +1,18 @@
+class ModelUser():
+    
+    @classmethod
+    def login(self,db,user):
+        try:
+            cursor = db.connection.cursor()
+            sql="""SELECT id, username, password, fullname FROM flask_login 
+                    WHERE username = '{}'""".format(user.username)
+            cursor.execute(sql)
+            row=cursor.fetchone()
+            if row != None:
+                user=User(row[0],row[1],User.check_password(row[2], user.password), row [3])
+            else: 
+                return None
+
+        
+        except Exception as ex:
+            raise Exception(ex) 
