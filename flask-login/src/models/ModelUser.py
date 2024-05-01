488e46fe13ef700d4752538e3e930803d36348bd
Anaid Olguin <aniiop03@gmail.com>
2024-04-30 18:50:47 -0600

    login correcto

--- flask-login/src/models/ModelUser.py
+++ flask-login/src/models/ModelUser.py
@@ -4,7 +4,7 @@ class ModelUser():
     def login(self,db,user):
         try:
             cursor = db.connection.cursor()
-            sql="""SELECT id, username, password, fullname FROM flask_login 
+            sql="""SELECT id, username, password, fullname FROM usuario 
                     WHERE username = '{}'""".format(user.username)
             cursor.execute(sql)
             row=cursor.fetchone()
@@ -16,3 +16,17 @@ class ModelUser():
         
         except Exception as ex:
             raise Exception(ex) 
+
+    @classmethod
+    def get_by_id(self, db, id):
+        try:
+            cursor = db.connection.cursor()
+            sql = "SELECT id, username, fullname FROM user WHERE id = {}".format(id)
+            cursor.execute(sql)
+            row = cursor.fetchone()
+            if row != None:
+                return User(row[0], row[1], None, row[2])
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)
