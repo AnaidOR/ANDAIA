2489305823beb830c3f95c6351e7f4ae9e447f67
Anaid Olguin <aniiop03@gmail.com>
2024-05-17 17:40:38 -0600

    ordena repo

--- flask-login/src/products.py
+++
@@ -1,6 +0,0 @@
-products = [ 
-    {"id": "1","nombre": "camisa", "precio ": 800, "cantidad": 4},
-    {"id":"2","nombre": "pantalom", "precio ": 80, "cantidad": 4},
-    {"id": "3", "nombre": "vestido", "precio ": 800, "cantidad": 4},
-   
-]
