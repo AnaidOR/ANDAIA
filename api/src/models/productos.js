090851ff1a6be8da2adbd8f905a59343d332c4fe
Anaid Olguin <aniiop03@gmail.com>
2024-05-19 23:20:04 -0600

    url,productos

--- api/src/models/productos.js
+++ api/src/models/productos.js
@@ -1,22 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const productosSchema = mongoose.Schema({
-    nombre: {
-        type: String,
-        required: true
-    },
-    descripcion: {
-        type: String,
-        required: true
-    },
-    precio: {
-        type: Number,
-        required: true
-    },
-    existencias: {
-        type: Number,
-        required: true
-    }
-});
+  nombre: {
+    type: String,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+  },
+  existencias: {
+    type: Number,
+    required: true,
+  },
+  imagen: {
+    type: String,
+    requiered: true,
+  },
+})
 
-module.exports = mongoose.model('Productos', productosSchema);
+module.exports = mongoose.model('Productos', productosSchema)
